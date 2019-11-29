3c13f4dc64896f0bd3e807100e707365fbdea799
Olya <oipelevanyuk@gmail.com>
2019-11-30 00:04:46 +0300

    iujh

--- components/Footer.js
+++ components/Footer.js
@@ -16,15 +16,40 @@ class Footer extends React.PureComponent{
                 </div>
                 
                 <div className='phoneBlock'>
-                    <span>
+                    <span className='phone-image'>
                         <img src='../images/phone.png' />
                     </span>
-                    <span>
+                    <span className='phone-number'>
                         <a href="tel:5148148">5-148-148</a>
                     </span>
-                    <span>A1 MTC Life:)</span>
+                    <span className='phone-type'>A1 MTC Life:)</span>
                 </div>
 
+                <div>
+                    <div>
+                        <a href='https://vk.com/bps_sberbank'>
+                            <img src='../images/vk.png' />
+                        </a>
+                        <a href='https://ok.ru/group/53654515155076'>
+                            <img src='../images/odn.png' />
+                        </a>
+                        <a href='https://www.facebook.com/sberbank.bps/'>
+                            <img src='../images/fb.png' />
+                        </a>
+                        <a href='https://www.instagram.com/bps_sberbank/?hl=ru'>
+                            <img src='../images/inst.png' />
+                        </a>
+                    </div>
+                    <div>Мобильный банк</div>
+                    <div></div>
+                </div>
+
+
+
+                <div className='copyright'>
+                    <span>© 1993—2019 ОАО «БПС-Сбербанк»</span>
+                    <a href='https://www.sberbank.ru'>сайт Сбербанка России</a>
+                </div>
             </footer>
         )
     }
