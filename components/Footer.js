cf75fcc64f852ac0ad26875ff02a0df5181271e5
Olya <oipelevanyuk@gmail.com>
2019-11-29 23:14:52 +0300

    wvrev

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.PureComponent{
+    render () {
+        return (
+            <footer>Footer place</footer>
+        )
+    }
+}
+
+export default Footer;
--- components/PagesRouter.js
+++ components/PagesRouter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import '../App.css'
+/*import '../App.css'
 
 import Page_Start from './pages/Page_Start';
 import Page_Products from './pages/Page_Products';
 import Page_Product from './pages/Page_Product';
 import Page_Basket from './pages/Page_Basket';
-import Page_Registration from './pages/Page_Registration';
+import Page_Registration from './pages/Page_Registration';*/
 
 class PagesRouter extends React.Component {
           
@@ -15,13 +15,14 @@ class PagesRouter extends React.Component {
   
       return (
         <div className='Route'>
-          <Route path="/" exact component={Page_Start} />
+          {/*<Route path="/" exact component={Page_Start} />
           <Route path="/depos" component={Page_Products} />
           <Route path="/kred" component={Page_Registration} />
           <Route path="/kard" component={Page_Basket} />
+          <Route path="/about" component={Page_Basket} />
           <Route path="/" component={Page_Start} />
           <Route path="/products" component={Page_Products} />
-          <Route path="/registration" component={Page_Registration} />
+      <Route path="/registration" component={Page_Registration} />*/}
         </div>
       );
       
