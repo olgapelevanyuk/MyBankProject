23c7c1ab2ce1f775e647d22ce407408842ea2f2f
Olya <oipelevanyuk@gmail.com>
2019-11-30 00:11:27 +0300

    sd

--- components/Footer.js
+++ components/Footer.js
@@ -5,44 +5,50 @@ class Footer extends React.PureComponent{
     render () {
         return (
             <footer className='footer'>
-                <div className='phoneBlock'>
-                    <span className='phone-image'>
-                        <img src='../images/148.png' />
-                    </span>
-                    <span className='phone-number'>
-                        <a href="tel:148">148</a>
-                    </span>
-                    <span className='phone-type'>Городской и мобильный номер</span>
-                </div>
-                
-                <div className='phoneBlock'>
-                    <span className='phone-image'>
-                        <img src='../images/phone.png' />
-                    </span>
-                    <span className='phone-number'>
-                        <a href="tel:5148148">5-148-148</a>
-                    </span>
-                    <span className='phone-type'>A1 MTC Life:)</span>
+                <div className='connection'>
+                    <div>
+
+                        <div className='phoneBlock'>
+                            <span className='phone-image'>
+                                <img src='../images/148.png' />
+                            </span>
+                            <span className='phone-number'>
+                                <a href="tel:148">148</a>
+                            </span>
+                            <span className='phone-type'>Городской и мобильный номер</span>
+                        </div>
+                        
+                        <div className='phoneBlock'>
+                            <span className='phone-image'>
+                                <img src='../images/phone.png' />
+                            </span>
+                            <span className='phone-number'>
+                                <a href="tel:5148148">5-148-148</a>
+                            </span>
+                            <span className='phone-type'>A1 MTC Life:)</span>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <div className='networks'>
+                            <a href='https://vk.com/bps_sberbank'>
+                                <img src='../images/vk.png' />
+                            </a>
+                            <a href='https://ok.ru/group/53654515155076'>
+                                <img src='../images/odn.png' />
+                            </a>
+                            <a href='https://www.facebook.com/sberbank.bps/'>
+                                <img src='../images/fb.png' />
+                            </a>
+                            <a href='https://www.instagram.com/bps_sberbank/?hl=ru'>
+                                <img src='../images/inst.png' />
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
-                <div>
-                    <div>
-                        <a href='https://vk.com/bps_sberbank'>
-                            <img src='../images/vk.png' />
-                        </a>
-                        <a href='https://ok.ru/group/53654515155076'>
-                            <img src='../images/odn.png' />
-                        </a>
-                        <a href='https://www.facebook.com/sberbank.bps/'>
-                            <img src='../images/fb.png' />
-                        </a>
-                        <a href='https://www.instagram.com/bps_sberbank/?hl=ru'>
-                            <img src='../images/inst.png' />
-                        </a>
-                    </div>
-                    <div>Мобильный банк</div>
-                    <div></div>
-                </div>
+                
 
 
 
