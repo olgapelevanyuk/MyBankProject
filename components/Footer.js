381fcf84bc0e75ba7150a12c663bcaa7de829b73
Olya <oipelevanyuk@gmail.com>
2019-11-29 23:37:57 +0300

    lkjn

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Footer.css';
 
 class Footer extends React.PureComponent{
     render () {
         return (
-            <footer>Footer place</footer>
+            <footer className='footer'>
+                <div className='phoneBlock'>
+                    <span className='phone-image'>
+                        <img src='../images/148.png' />
+                    </span>
+                    <span className='phone-number'>
+                        <a href="tel:148">148</a>
+                    </span>
+                    <span className='phone-type'>Городской и мобильный номер</span>
+                </div>
+                
+                <div className='phoneBlock'>
+                    <span>
+                        <img src='../images/phone.png' />
+                    </span>
+                    <span>
+                        <a href="tel:5148148">5-148-148</a>
+                    </span>
+                    <span>A1 MTC Life:)</span>
+                </div>
+
+            </footer>
         )
     }
 }
