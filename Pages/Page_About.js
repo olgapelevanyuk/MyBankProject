75c8d5771d2ac20ec6f08123d9a0614e00828f98
Olya <oipelevanyuk@gmail.com>
2019-11-30 23:07:34 +0300

    ds

---
+++ Pages/Page_About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Page_About.css';
+
+export default class Page_About extends React.PureComponent {
+
+    render() {
+        return(
+            <div>About</div>
+        )
+    }
+}
