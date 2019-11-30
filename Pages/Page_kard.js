7757385631cc79293dc141d5ef23f8628cfc94da
Olya <oipelevanyuk@gmail.com>
2019-11-30 22:45:37 +0300

    kjn

--- Pages/Page_kard.js
+++ Pages/Page_kard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Page_Depos.css";
+import Product from '../components/Product';
 export default class Page_kard extends React.PureComponent {
 
     static defaultProps = {
@@ -11,6 +12,15 @@ export default class Page_kard extends React.PureComponent {
                 term: '10 BYN',
                 rate: 'Мобильный банк',
                 min: 'Карточка оформляется детям до 18 лет',
+                fullInfo: [
+                    'Карточка оформляется детям до 18 лет',
+                    'Система обеспечения безопасности оплаты товаров и услуг в сети Интернет',
+                    'Получайтe на свой мобильный телефон SMS с информацией о каждой операции',
+                    'Цифровой кошелек, позволяющий совершать покупки при помощи телефона',
+                    'Бесплатное пополнение карты Банка в BYN через АИС "Расчет"',
+                    'Совершение платежей с помощью Вашего компьютера',
+                ]
+
             },
             {
                 title: 'Mastercard World ComPass Premium в белорусских рублях',
@@ -19,6 +29,13 @@ export default class Page_kard extends React.PureComponent {
                 term: 'от 150 BYN',
                 rate: 'Мильная программа 1 EUR = 1 миля',
                 min: 'money-back 0.9%',
+                fullInfo: [
+                    'Цифровой кошелек, позволяющий совершать покупки при помощи телефона',
+                    'Бесплатное пополнение карты Банка в BYN через АИС "Расчет"',
+                    'За каждый потраченный 1 EUR вы получаете 1 бонусную милю в кабинет Skyclub',
+                    'Услуга технической помощи на дороге на всей территории Республики Беларусь',
+                    'Консьерж-Сервис – это личный ассистент, который всегда готов выполнить поручения Клиента в любое время суток',
+                ]
             },
             {
                 title: 'Visa Platinum PayWave',
@@ -27,13 +44,38 @@ export default class Page_kard extends React.PureComponent {
                 term: 'от 100 BYN',
                 rate: 'Консьерж-сервис',
                 min: 'Lounge Key',
+                fullInfo: [
+                    'Lounge Key – международная программа, обеспечивающая Вам легкий и быстрый доступ более чем в 1000 lounge-залов ожидания аэропортов по всему миру',
+                    'Консьерж-Сервис – это личный ассистент, который всегда готов выполнить поручения Клиента в любое время суток',
+                    'Воспользуйтесь специальными предложениями от платежной системы Visa',
+                    'Страхование выезжающих за рубеж - 1 год, 20 дней пребывания, 30 000 €',
+                ]
             },
         ]
     }
 
+    state={
+        workMode: 1, // 1 - все продукты, 2 - подробное описание выбранного продукта
+        chosenProd: {},
+    }
+
+    showProduct = (prod) =>{
+        this.setState({
+            workMode: 2,
+            chosenProd: prod,
+        })
+    }
+
+    closeProduct = () => {
+        this.setState({
+            workMode: 1,
+            chosenProd: {},
+        })
+    }
+
 
     render() {
-        let {props: {depos}} = this;
+        let {props: {depos}, state: {workMode, chosenProd}, showProduct} = this;
 
         let deposits = depos.map((item,idx) => {
             return (
@@ -59,7 +101,7 @@ export default class Page_kard extends React.PureComponent {
                         </div>
                     </div>
                     <div className='prod-buttons'>
-                        <div className='fullInfo'>Подробнее</div>
+                        <div className='fullInfo' onClick={()=>showProduct(item)}>Подробнее</div>
                         <div className='orderCall'>Заказать карту</div>
                     </div>
                 </div>
@@ -70,9 +112,13 @@ export default class Page_kard extends React.PureComponent {
             <div className='Page_Depos'>
                 <div className='banner' style={{backgroundImage: 'url("../images/depos.jpg")'}}></div>
 
-                <div>
+                {workMode === 1 && <div>
                     {deposits}
-                </div>
+                </div>}
+
+                {workMode === 2 && 
+                    <Product data={chosenProd}/>
+                }
 
 
 
