b90b33d5af91bf615d57b40ec52434ddf35d4391
Olga Pelevaniuk <oiapelevaniuk@gmail.com>
2019-12-01 15:16:48 +0300

    egergege
    Signed-off-by: Olga Pelevaniuk <oiapelevaniuk@gmail.com>

--- components/ApplicationList.js
+++ components/ApplicationList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {acApplicationsLoaded, APPLICATIONS_LOADING, acUpdateApplication, acSetCurrentApplication} from '../constants/actionTypes';
+import {acApplicationsLoaded, APPLICATIONS_LOADING, acUpdateApplication, acSetCurrentApplication, USERS_LOADING, USERS_LOADED} from '../constants/actionTypes';
 import {withRouter} from 'react-router-dom';
 import { getData, patchData } from '../utils/utils';
 import '../components/ApplicationList.css'; 
@@ -8,16 +8,44 @@ class ApplicationList extends React.PureComponent{
 
     async componentDidMount(){
         this.props.dispatch({
+            type: USERS_LOADING,
+        });
+            let usersList = await getData('users');
+        this.props.dispatch({
+            type: USERS_LOADED,
+            data: usersList,
+        });
+        this.props.dispatch({
             type: APPLICATIONS_LOADING,
         });
             let data = await getData('applications');
         this.props.dispatch(acApplicationsLoaded(data));           
-    }        
+    }     
+    
+    initialState = {
+        currentApplication: {id: null},
+        operatorsListOpen: false,
+    }
+
+    state = {...this.initialState}
+
+    
 
     changeApplStatus = async (applId, value) => {
         let application = await patchData('applications/' + applId, {status: value});
         this.props.dispatch(acUpdateApplication(application));
     }
+
+    assignOperator = async (operator) => {
+        const changes = {
+            status: 2,
+            operator: operator,
+        };
+        let application = await patchData('applications/' + this.state.currentApplication.id, changes);
+        this.props.dispatch(acUpdateApplication(application));
+        this.hideOperatorsList();
+    } 
+
     renderClientApplications = () => {
         const {list} = this.props.applications;
         if(!list.length){
@@ -38,18 +66,38 @@ class ApplicationList extends React.PureComponent{
                 {appl.status === 2 && "Взята в работу"}
                 {appl.status === 3 && "Исполнена"}
             </div>
+
+            <div>
             <div>
                 <button onClick={this.showApplicationFullInfo.bind(this, appl.id)}>Просмотреть полную информацию</button>
             </div>
             <div>
+                {appl.status === 1 && (this.props.users.currentUser && this.props.users.currentUser.userProfile.type === 'operator') &&
                 <button onClick={this.changeApplStatus.bind(this, appl.id, 2)}>Взять в работу</button>
+        }
+        {
+        appl.status === 1 && (this.props.users.currentUser && this.props.users.currentUser.userProfile.type === 'admin') &&
+                <button onClick={this.showOperatorsList.bind(this, appl.id)}>Передать в работу</button>
+        }
             </div>
             <div>
                 <button onClick={this.changeApplStatus.bind(this, appl.id, 3)}>Пометить как исполненную</button>
             </div>
+            </div>
                 </div>
             ))
         }
+    }
+
+    renderOperatorsList = () => {
+        const operatorsList = this.props.users.list.filter(user => user.type === 'operator');
+        return operatorsList.map(operator => (
+            <div onClick={this.assignOperator.bind(this, operator)}>
+                <div>
+                    {`${operator.surname} ${operator.firstName} ${operator.LastName}`}
+                </div>
+            </div>
+        ))
     }
 
     showApplicationFullInfo = async id => {
@@ -59,17 +107,43 @@ class ApplicationList extends React.PureComponent{
         this.props.history.push(`applications/${id}`);
     }
 
+    hideOperatorsList = () => {
+        this.setState({
+            currentApplication: {id: null},
+        operatorsListOpen: false,
+        })
+    }
+
+    showOperatorsList = applId => {
+        this.setState({
+            currentApplication: {id: applId},
+        operatorsListOpen: true,
+        })
+    }
+
     render () {
         
         
 
         return (
-            this.props.applications.loading && 
+            (this.props.applications.loading || this.props.users.loading) && 
             (<div>Идет загрузка</div>) || 
             (
+                <div>
             <div>
                 <h2>Заявки клиентов</h2>
                 {this.renderClientApplications()}
+            </div>
+            { this.state.operatorsListOpen && 
+            <div class="operatorsListContainer">
+                <div>
+                    {this.renderOperatorsList()}
+                </div>
+                <div>
+                    <button onClick={this.hideOperatorsList}>Отменить выбор оператора</button>
+                </div>
+            </div>
+    }
             </div>
             )
         )
